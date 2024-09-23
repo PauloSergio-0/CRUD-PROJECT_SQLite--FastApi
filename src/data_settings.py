6cf106b4c8a5712a8ce5361d143202c1d14884ef
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-09-23 16:31:57 -0300

    add functions t sqlite

--- src/data_settings.py
+++ src/data_settings.py
@@ -25,16 +25,6 @@ sql_clientes = """
     );
 """
 
-insere_cliente  = """
-    INSERT INTO Cliente VALUES 
-    ('12345678901', 'Carlos', 'Silva', '1234567890', 'Rua das Flores', '123', 'Centro'),
-    ('98765432109', 'Ana', 'Souza', '0987654321', 'Avenida Brasil', '456', 'Jardim América'),
-    ('56789012345', 'Marcos', 'Pereira', '1122334455', 'Rua dos Pinheiros', '789', 'Vila Nova'),
-    ('23456789012', 'Luana', 'Oliveira', '2345678901', 'Rua das Laranjeiras', '321', 'Bela Vista'),
-    ('34567890123', 'Ricardo', 'Martins', '3456789012', 'Avenida das Palmeiras', '654', 'Jardim São Paulo'),
-    ('45678901234', 'Fernanda', 'Alves', '4567890123', 'Praça da Liberdade', '987', 'Centro Histórico'),
-    ('56789012346', 'Tiago', 'Gomes', '5678901234', 'Rua do Comércio', '852', 'Vila Esperança');
-    """
 sql_vendas = """
     CREATE TABLE IF NOT EXISTS Venda (
     ID_transacao INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -76,9 +66,72 @@ def verificar_db():
         print(f'Ocorreu um erro: {e}')
 
 
-make_db()
-verificar_db()
+def inserir_dados_vendas():
+    inserer_vendas = """
+    INSERT INTO Venda (Nota_Fiscal, ID_Cliente, Data_compra, ID_Veiculo, Quantidade) 
+    VALUES 
+    (123, 2, '2024-12-08', 2, 1),
+    (123, 1, '2024-12-08', 3, 1),
+    (123, 3, '2024-12-08', 1, 3),
+    (123, 4, '2024-12-08', 2, 1);
+    """
+    
+    try:
+        curso.execute(inserer_vendas)
+        
+        Conexao.commit()
+        
+    except con.DatabaseError as e:
+        print(f'Ocorreu um erro: {e}')
+        
+        
+def inserir_dados_carros():
+    insere_carro = """
+    INSERT INTO Veiculo (Marca_Veiculo, Modelo_Veiculo, Preco_Veiculo, Ano_Veiculo, Qtde_estoque) VALUES 
+    ('Ferrari', 'Enzo', 123.67, 1992, 23),
+    ('Ferrari', 'La_Ferrari', 167.90, 1962, 3),
+    ('Porshe', '911s', 902.22,1990, 8);
+    """
+    try:
+        curso.execute(insere_carro)
+        Conexao.commit()
+    except con.DatabaseError as e:
+        print(f"Ocorreu um erro: {e}")
+        
 
+def inserir_dados_Clientes():
+    insere_cliente  = """
+        INSERT INTO Cliente (RG, Nome_cliente, Sobrenome_cliente , Telefone, Rua, Numero, Bairro) VALUES 
+        ('12345678901', 'Carlos', 'Silva', '1234567890', 'Rua das Flores', '123', 'Centro'),
+        ('98765432109', 'Ana', 'Souza', '0987654321', 'Avenida Brasil', '456', 'Jardim América'),
+        ('56789012345', 'Marcos', 'Pereira', '1122334455', 'Rua dos Pinheiros', '789', 'Vila Nova'),
+        ('23456789012', 'Luana', 'Oliveira', '2345678901', 'Rua das Laranjeiras', '321', 'Bela Vista'),
+        ('34567890123', 'Ricardo', 'Martins', '3456789012', 'Avenida das Palmeiras', '654', 'Jardim São Paulo'),
+        ('45678901234', 'Fernanda', 'Alves', '4567890123', 'Praça da Liberdade', '987', 'Centro Histórico'),
+        ('56789012346', 'Tiago', 'Gomes', '5678901234', 'Rua do Comércio', '852', 'Vila Esperança');
+    """
+    try:
+        curso.execute(insere_cliente)
+        Conexao.commit()
+    except con.DatabaseError as e:
+        print(f'Ocorreu um erro: {e}')
+
+
+def listar_dados(tabela = 'Cliente'):
+    listar_tabela = f"SELECT * FROM {tabela};"
+    try:
+        curso.execute(listar_tabela)
+        res = curso.fetchall()
+        for row in res:
+            print(row)
+    except con.DatabaseError as e:
+        print(f'Ocorreu um erro: {e}')
+make_db()
+# verificar_db()
+# inserir_dados_Clientes()
+# inserir_dados_carros()
+# inserir_dados_vendas()
+listar_dados(tabela='Veiculo')
 global Conexao
 if Conexao:
     Conexao.close()
