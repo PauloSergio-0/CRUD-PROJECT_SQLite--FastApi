98138d6a691cb4df37004fec77e62c9aae1c2859
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-09-24 11:16:19 -0300

    add files

--- src/domain/file_processor.py
+++ src/domain/file_processor.py
@@ -1,12 +1,17 @@
 from fastapi import UploadFile, HTTPException, status
 import pandas as pd 
-
+import sqlite3 as con
 
 
 class File_manipulation:
     def __init__(self):
-        self.file_path = 'data/veiculo.csv'
+        self.file_path = 'data/veiculo.db'
         self.directory = 'data'
         
         
-    
+    def Create_db(self):
+        sql_create = """
+        CREATE TABLE IF NOT EXISTS Carro (
+            
+        )
+        """
